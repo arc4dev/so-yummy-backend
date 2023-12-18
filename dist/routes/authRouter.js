9e7b5b1e6dd3381dce68b7cefea93d562308caef
Arkadiusz Sroczyk <117762248+arc4dev@users.noreply.github.com>
2023-12-18 12:56:53 +0100

    Delete dist directory

--- dist/routes/authRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import authController from '../controllers/authController.js';
-const authRouter = express.Router();
-authRouter.post('/sign-up', authController.signUp);
-authRouter.post('/sign-in', authController.signIn);
-authRouter.post('/sign-out', authController.signOut);
-authRouter.get('/verify/:verificationToken', authController.verifyUser);
-authRouter.post('/verify', authController.resendVerificationEmail);
-export default authRouter;
--- dist/routes/userRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import authController from '../controllers/authController.js';
-import userController from '../controllers/userController.js';
-const userRouter = express.Router();
-// Auth before all requests
-userRouter.use(authController.auth);
-userRouter.get('/current', userController.getCurrentUser);
-export default userRouter;
